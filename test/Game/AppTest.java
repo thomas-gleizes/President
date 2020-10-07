e9dd9db67e31e2fcc14022526b67ebafcfbcde1e
thomas-gleizes <47100219+thomas-gleizes@users.noreply.github.com>
2020-10-07 10:38:02 +0200

    Init

---
+++ test/Game/AppTest.java
@@ -0,0 +1,14 @@
+package Game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+
+    @Test
+    void mainTest() {
+        assertEquals(1, 1);
+    }
+
+}
