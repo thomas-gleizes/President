561ec753a42f275d5a89533b1e37229beca773d7
thomas <thomas-gleizes>
2020-10-26 09:40:53 +0100

    modify (add DECK)

--- src/Game/App.java
+++ src/Game/App.java
@@ -3,7 +3,7 @@ package Game;
 public class App {
 
     public static void main(String[] args) {
-        Board game = new Board(new String[]{"Silver", "Gold", "Cooper", "Tin"});
+        Game game = new Game(new String[]{"Silver", "Gold", "Cooper", "Tin"});
         System.out.println(game);
     }
 
